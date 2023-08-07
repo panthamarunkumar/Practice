7612f4bc939da22464c7bf4f2e271d22a4bbecd8
panthamarunkumar <70441456+panthamarunkumar@users.noreply.github.com>
2023-08-07 23:44:17 +0530

    dkek2

--- gitdemoapp/gitdemoapp/Controllers/WeatherForecastController.cs
+++ gitdemoapp/gitdemoapp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace gitdemoapp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warhgyhm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warhgyhm", "Badjeklmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
